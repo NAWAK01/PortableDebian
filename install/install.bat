2455688b924c50f801616f0296627a47d2137f72
NAWAK01 <30952533+NAWAK01@users.noreply.github.com>
2018-09-23 11:15:54 +0200

    Update install.bat

--- install/install.bat
+++ install/install.bat
@@ -1,1 +1,11 @@
 
+@echo off
+set /a size = 20
+echo %size%
+start bin/qemu-img.exe create hdd.img %size%G
+echo a %size%Go virtual disk has been created
+setx ram_for_vm 2048
+setx cpu_for_vm 4
+pause
+move hdd.img media/hdd.img
+pause
